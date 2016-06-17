e29fdb16a7d470a072a3d5f2c900896c3c867d4d
Zoltán Kornél Török <taz19880922@gmail.com>
2016-06-17 16:02:16 +0200

    initial commit, create spring boot application, what we want to use for demo

---
+++ autoScalingDemo/src/main/java/com/practice/aws/AutoScalingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.practice.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoScalingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoScalingDemoApplication.class, args);
+	}
+}
